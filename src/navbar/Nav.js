8f48ec70ca7094ccb842e5da11ef4bc8fd962ff4
Divakar Yadav <divakary1110@gmail.com>
2022-04-07 16:55:08 +1200

    Teachers profile page

---
+++ src/navbar/Nav.js
@@ -0,0 +1,17 @@
+import "./Nav.css"
+//import Imagelogo from "./components/navbar/Star Logo 07-2.png"
+function Nav() {
+    return(
+        <div className="Menu">
+            <div className="Navbar">
+                <div className="Logo"></div>
+                    <h3>Home</h3>
+                    <h3 id="Project">Projects</h3>
+                    <h3 id="Teacher">Teacher</h3>
+            </div>
+
+        </div>
+    )
+}
+
+export default Nav;
